9c0e926b5845fa82217287920987f715add5162c
Pholisa-Fatyela <pholisa@projectcodex.co>
2016-01-26 15:36:42 +0200

    created a seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 Job.create!([
-  {title: "Chef", description: "pastries", requirements: "good with intsruction", job_level: "junior chef", status: "full time", start_date: "2016-01-29", contact: "0216563984\r\nMichelle"},
-  {title: "Nurse", description: "will work with all patients at the hospital", requirements: "Patience", job_level: "Health worker", status: "Full-time", start_date: "2016-01-26", contact: "Patho 0215548769"},
-  {title: "Programmer", description: "coding and working on projects", requirements: "Coding skills. Eager to learn. Communicative", job_level: "Intern", status: "Full-time", start_date: "2016-01-26", contact: "Mark 0782254698"},
-  {title: "Dog trainer", description: "Walk the dog. Wash and take care of all its grooming needs", requirements: "Good with dogs", job_level: "Dog handler", status: "full time", start_date: "2016-03-16", contact: "Pearl 0214587612"},
-  {title: "gdsg", description: "gsdg", requirements: "gdsgsd", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "fdgdfgd", description: "gdfgdfagfdag", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "fdgdfgfg", description: "gdfgfdg", requirements: "dfgdfgdfgfdg", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "lkjhgfdsa", description: "hgfdsa`", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "xfhjhjljhl", description: "fjhljklokj;", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "zfxukuy", description: "kjlkmlij,km", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "qwerty", description: "qwerty", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
-  {title: "zwethu", description: "zorro", requirements: "", job_level: "", status: "", start_date: nil, contact: ""}
+  {title: "dog handler", description: "dfgh", requirements: "", job_level: "hvds", status: "", start_date: nil, contact: ""},
+  {title: "gardener", description: "flowers", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "game tester", description: "ps3", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Programmer", description: "ruby", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Singer", description: "country music\r\n", requirements: "\r\n", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Chef", description: "food", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Agile coach", description: "able to work with scrum", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Receptionist", description: "Answer calls", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Baker", description: "Awesome with pastries", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Mentor", description: "Good with people", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Intern", description: "eager to learn and always ask questions", requirements: "", job_level: "", status: "", start_date: nil, contact: ""},
+  {title: "Nurse", description: "Will take care of sick people", requirements: "Associate's Degree in Nursing and NYS RN license\r\nMust have daily access to a car that is insured in your name and possess and maintain a valid driver's license; proof of all is required\r\nBe able to travel within the 5 boroughs as assigned.  This position reports to the headquarters location in lower Manhattan.\r\nU.S. based experience of which at least one year is in one of the following areas:\r\nGeriatrics\r\nHome Care\r\nDischarge Planning\r\nCase Management\r\nMedical Surgical Nursing", job_level: "Health worker", status: "Full-time", start_date: "2016-01-25", contact: "Madam Peru\r\n0213631258"}
 ])

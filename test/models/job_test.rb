14fd26801a52df6f5cdf3cbec9a59814a8d8ecc3
Pholisa-Fatyela <pholisa@projectcodex.co>
2016-02-24 12:23:35 +0200

    Fixed test

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,15 +1,21 @@
 require "test_helper"
 
 class JobTest < MiniTest::Test
-  def test_validation
+  def test_validation_title
     job = Job.new
     job.valid?
     assert job.errors.keys.include?(:description), "Saved the job without a title"
   end
 
-  def  test_job_taggings
-    t = Job.reflect_on_association(:taggings)
-    assert_equal :has_many
+  def test_validation_description
+  	job = Job.new
+		job.valid?
+	 	assert job.errors.keys.include?(:description), "Saved the job without description"
   end
+
+  # def  test_job_taggings
+  #   t = Job.reflect_on_association(:taggings)
+  #   assert_equal :has_many
+  # end
 
 end
